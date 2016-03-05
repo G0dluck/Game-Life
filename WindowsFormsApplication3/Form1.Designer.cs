9067dc15a373e10d59abf5fed1a6352c131e406d
G0dluck <chekynov23@mail.ru>
2016-03-05 17:31:09 +0300

    Release
    Ver. 1.0

---
+++ WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.run_btn = new System.Windows.Forms.Button();
+            this.stop_btn = new System.Windows.Forms.Button();
+            this.next_Step = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 41);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(301, 301);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            // 
+            // run_btn
+            // 
+            this.run_btn.Location = new System.Drawing.Point(12, 12);
+            this.run_btn.Name = "run_btn";
+            this.run_btn.Size = new System.Drawing.Size(75, 23);
+            this.run_btn.TabIndex = 1;
+            this.run_btn.Text = "Run";
+            this.run_btn.UseVisualStyleBackColor = true;
+            this.run_btn.Click += new System.EventHandler(this.run_btn_Click);
+            // 
+            // stop_btn
+            // 
+            this.stop_btn.Location = new System.Drawing.Point(93, 12);
+            this.stop_btn.Name = "stop_btn";
+            this.stop_btn.Size = new System.Drawing.Size(75, 23);
+            this.stop_btn.TabIndex = 2;
+            this.stop_btn.Text = "Stop";
+            this.stop_btn.UseVisualStyleBackColor = true;
+            this.stop_btn.Click += new System.EventHandler(this.stop_btn_Click);
+            // 
+            // next_Step
+            // 
+            this.next_Step.Location = new System.Drawing.Point(174, 12);
+            this.next_Step.Name = "next_Step";
+            this.next_Step.Size = new System.Drawing.Size(75, 23);
+            this.next_Step.TabIndex = 3;
+            this.next_Step.Text = "Next step";
+            this.next_Step.UseVisualStyleBackColor = true;
+            this.next_Step.Click += new System.EventHandler(this.next_Step_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(255, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 357);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.next_Step);
+            this.Controls.Add(this.stop_btn);
+            this.Controls.Add(this.run_btn);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Game - Life";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button run_btn;
+        private System.Windows.Forms.Button stop_btn;
+        private System.Windows.Forms.Button next_Step;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            label1.Text = "Gen: " + generation.ToString();
+
+            timer = new Timer();
+            timer.Interval = 100;
+            timer.Tick += timer_Tick;
+        }
+
+        Timer timer;
+
+        Graphics g;
+        BufferedGraphicsContext bufferedGraphicsContext;
+        BufferedGraphics bufferedGraphics;
+
+        int rec_Width;
+        int rec_Height;
+        const int field_size = 30;
+        int generation = 0;
+
+        bool[,] arrLife = new bool[field_size, field_size];
+        bool[,] arrLifeTemp = new bool[field_size, field_size];
+
+        int countValue = field_size;
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int x = (e.X / rec_Width);
+            int y = (e.Y / rec_Height);
+
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                bufferedGraphics.Graphics.FillRectangle(Brushes.Green, 
+                    x * rec_Width + 2, y * rec_Height + 2, rec_Width - 3, rec_Height - 3);
+                arrLife[x, y] = true;
+            }
+            else
+            {
+                bufferedGraphics.Graphics.FillRectangle(SystemBrushes.Control,
+                    x * rec_Width + 2, y * rec_Height + 2, rec_Width - 3, rec_Height - 3);
+                arrLife[x, y] = false;
+            }
+
+            bufferedGraphics.Render();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = Graphics.FromHwnd(panel1.Handle);
+            bufferedGraphicsContext = new BufferedGraphicsContext();
+            bufferedGraphics = bufferedGraphicsContext.Allocate(g, 
+                new Rectangle(0, 0, panel1.Size.Width, panel1.Size.Height));
+            bufferedGraphics.Graphics.Clear(BackColor);
+            Pen p = new Pen(Color.Black);
+            rec_Width = panel1.Width / field_size;
+            rec_Height = panel1.Height / field_size;
+            for (int i = 0; i <= panel1.Width; i += rec_Width)
+                for (int j = 0; j <= panel1.Height; j += rec_Height)
+                {
+                    bufferedGraphics.Graphics.DrawRectangle(p, i, j, rec_Width, rec_Height);
+                }
+
+            bufferedGraphics.Render();
+        }
+
+        private void GoLife()
+        {
+            for (int i = 0; i < countValue; i++)
+                for (int j = 0; j < countValue; j++)
+                {
+                    int sosedi = Sosedi(i, j);
+
+                    if (sosedi == 3 && (!arrLife[i, j])) { arrLifeTemp[i, j] = true; }
+                    if ((sosedi == 3 || sosedi == 2) && arrLife[i, j]) { arrLifeTemp[i, j] = true; }
+                    if (sosedi < 2) { arrLifeTemp[i, j] = false; }
+                    if (sosedi > 3) { arrLifeTemp[i, j] = false; }
+                }
+
+            for (int i = 0; i < countValue; i++)
+                for (int j = 0; j < countValue; j++)
+                {
+                    if (arrLife[i, j] != arrLifeTemp[i, j])
+                    {
+                        arrLife[i, j] = arrLifeTemp[i, j];
+                        if (arrLife[i, j])
+                        {
+                            bufferedGraphics.Graphics.FillRectangle(Brushes.Green, 
+                                i * rec_Width + 2, j * rec_Height + 2, rec_Width - 3, rec_Height - 3);
+                        }
+                        else
+                        {
+                            bufferedGraphics.Graphics.FillRectangle(SystemBrushes.Control, 
+                                i * rec_Width + 2, j * rec_Height + 2, rec_Width - 3, rec_Height - 3);
+                        }
+
+                        bufferedGraphics.Render();
+                    }
+                }
+
+            generation++;
+            label1.Text = "Gen: " + generation.ToString();
+        }
+
+        private int Sosedi(int i, int j)
+        {
+            int sosedi = 0;
+            int limit = countValue - 1;
+
+            if (i > 0 && arrLife[i - 1, j]) { sosedi++; }
+            if (i > 0 && j > 0 && arrLife[i - 1, j - 1]) { sosedi++; }
+            if (i > 0 && j < countValue - 1 && arrLife[i - 1, j + 1]) { sosedi++; }
+            if (j < countValue - 1 && arrLife[i, j + 1]) { sosedi++; }
+            if (j > 0 && arrLife[i, j - 1]) { sosedi++; }
+            if (i < countValue - 1 && arrLife[i + 1, j]) { sosedi++; }
+            if (i < countValue - 1 && j < countValue - 1 && arrLife[i + 1, j + 1]) { sosedi++; }
+            if (i < countValue - 1 && j > 0 && arrLife[i + 1, j - 1]) { sosedi++; }
+
+            return sosedi;
+        }
+
+        private void run_btn_Click(object sender, EventArgs e)
+        {
+            timer.Start();
+
+            run_btn.Enabled = false;
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            GoLife();
+        }
+
+        private void stop_btn_Click(object sender, EventArgs e)
+        {
+            if (timer.Enabled)
+            {
+                timer.Stop();
+                run_btn.Enabled = true;
+            }
+        }
+
+        private void next_Step_Click(object sender, EventArgs e)
+        {
+            if (timer.Enabled)
+            {
+                timer.Stop();
+                run_btn.Enabled = true;
+            }
+
+            GoLife();
+        }
+    }
+}
